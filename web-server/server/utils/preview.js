344bd0a2dfed94b9b227d29103a052442813da35
liufu.lf <liufu.lf@antfin.com>
2021-05-24 16:30:42 +0800

    fix: 优化代码

--- web-server/server/utils/preview.js
+++ web-server/server/utils/preview.js
@@ -2,13 +2,14 @@ const shell = require('shelljs');
 const fs = require('fs');
 
 const useStatic = async () => {
+  const basePath = '../../public/preview';
   // 删除 preview 静态文件
-  if (!fs.existsSync('../../public/preview/')) {
-    shell.exec('mkdir -p ../../public/preview');
+  if (!fs.existsSync('/')) {
+    shell.exec(`mkdir -p ${basePath}`);
   } else {
-    shell.exec('rm -rf ../../public/preview/*');
+    shell.exec(`rm -rf ${basePath}/*`);
   }
-  shell.exec('cp -r ./static/* ../../public/preview/');
+  shell.exec(`cp -r ./static/* ${basePath}/`);
   process.exit();
 };
 
