f7a49b62134fcad4adf4aa33c092d3506ac4a4b0
liufu.lf <liufu.lf@antfin.com>
2021-05-24 15:44:23 +0800

    feat: async

--- web-server/server/utils/preview.js
+++ web-server/server/utils/preview.js
@@ -1,7 +1,9 @@
-const shell = require('shelljs')
+const shell = require('shelljs');
 
 const useStatic = async () => {
   // 删除 preview 静态文件
+  shell.exec('pwd');
+  shell.exec('ls');
   shell.exec('rm -rf ../../public/preview/*');
   shell.exec('cp -r ./static/* ../../public/preview/');
   process.exit();
