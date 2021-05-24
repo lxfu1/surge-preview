6be07290e6370b8ab63fbaf60313bc70dbef3f30
liufu.lf <liufu.lf@antfin.com>
2021-05-24 19:19:52 +0800

    fear: build code

--- dist/index.js
+++ dist/index.js
@@ -210,6 +210,7 @@ function build() {
         return new Promise((resovle) => __awaiter(this, void 0, void 0, function* () {
             const project_name = core.getInput('project_name') || 'G2Plot';
             const project_branch = core.getInput('project_branch') || 'master';
+            yield exec_1.exec(`ls`);
             yield exec_1.exec(`npx sh start.sh ${project_name} ${project_branch}`);
             yield exec_1.exec(`ls ./public/preview`);
             resovle(null);
